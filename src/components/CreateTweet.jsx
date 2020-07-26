f4e43f363b925193210460f2c7dfd1675cc661d8
Becca <60694049+olive49@users.noreply.github.com>
2020-07-26 17:45:35 +0300

    Milestone 1

--- src/components/CreateTweet.jsx
+++ src/components/CreateTweet.jsx
@@ -1,7 +1,4 @@
 import React, { Component } from "react";
-// import TextField from "@material-ui/core/TextField";
-// import Card from "@material-ui/core/Card";
-// import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 
 class CreateTweet extends Component {
